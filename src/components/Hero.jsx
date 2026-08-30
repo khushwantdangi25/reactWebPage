9202e86898abb1068ac93fc14cdf1baceecde619
khushwant <khushwantdangi25@gmail.com>
2026-08-30 22:28:40 +0530

    hero-section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+import { About } from "../pages/About";
+export const Hero = () => {
+    return <main>
+        <div className="hero">
+            <div className="quote">
+                <h1>
+                    “The world is a book, <br />and those who do not <br /> travel
+                    read only one page.”
+                </h1>
+                <p>They can inspire planning a new trip, embracing spontaneity,<br />
+                    or simply appreciating the experiences gained from past journeys.</p>
+                <NavLink to="/country" className="explore-btn">Explore</NavLink>
+            </div>
+            <div className="image">
+                <img src="https://th.bing.com/th/id/OIP.bahcsh5zMhWFZtqvoVHnrwHaEK?r=0&pid=CanonicalCanvas"
+                    alt="" />
+            </div>
+        </div>
+        <About />
+    </main>
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,7 @@
-import { NavLink } from "react-router-dom";
+
 import "./Home.css";
+import { Hero } from "../components/Hero";
 
 export const Home = () => {
-  return (
-    <main>
-      <div className="hero">
-        <div className="quote">
-          <h1>
-          “The world is a book, <br />and those who do not <br /> travel
-          read only one page.”
-          </h1>
-          <p>They can inspire planning a new trip, embracing spontaneity,<br />
-           or simply appreciating the experiences gained from past journeys.</p>
-           <NavLink to="/country" className="explore-btn">Explore</NavLink>
-        </div>
-        <div className="image">
-          <img src="https://th.bing.com/th/id/OIP.bahcsh5zMhWFZtqvoVHnrwHaEK?r=0&pid=CanonicalCanvas" 
-          alt="" />
-        </div>
-      </div>
-    </main>
-  );
+  return <Hero />
 };
